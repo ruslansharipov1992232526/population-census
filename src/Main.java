185cdfcd36aaf08c183a958e4c245c499ffe23f3
Ruslan Sharipov <ruslansharipov@Ruslans-MacBook-Pro.local>
2022-11-21 15:39:44 +0500

    Update files

--- src/Main.java
+++ src/Main.java
@@ -16,22 +16,34 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
+        System.out.println("Поиск несовершеннолетних");
         // Поиск несовершеннолетних
         long count = persons.stream()
                 .filter(x -> x.getAge() < 18)
                 .count();
         System.out.println(count);
 
+        System.out.println("Поиск призывников");
         // Поиск призывников
         persons.stream()
-                .filter(w -> w.getAge() > 18 && w.getAge() < 27)
+                .filter(w -> w.getAge() > 18 && w.getAge() <= 27 && w.getSex().equals(Sex.MAN))
                 .map(Person::getFamily)
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
 
+        System.out.println("Женщины с образованием от 18 до 60");
         // Поиск людей с образованием
         persons.stream()
-                .filter(w -> w.getAge() > 27)
+                .filter(w -> w.getAge() > 18 && w.getAge() < 60 && w.getSex().equals(Sex.WOMAN))
+                .sorted(Comparator.comparing(Person::getEducation))
+                .map(Person::getFamily)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+        System.out.println("Мужчины с образованием от 18 до 65");
+        // Поиск людей с образованием
+        persons.stream()
+                .filter(w -> w.getAge() > 18 && w.getAge() < 65 && w.getSex().equals(Sex.MAN))
                 .sorted(Comparator.comparing(Person::getEducation))
                 .map(Person::getFamily)
                 .collect(Collectors.toList())
